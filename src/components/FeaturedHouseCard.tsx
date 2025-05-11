28554e1d1ed7d690b469af8e7c153ca0c02b5738
Merab Tskhvedadze <merabi.tskhvedadze@gmail.com>
2025-05-11 13:03:04 -0700

    add featured house card - WIP

---
+++ src/components/FeaturedHouseCard.tsx
@@ -0,0 +1,23 @@
+import { Card, CardContent, Typography, Button } from '@mui/material';
+
+export default function FeaturedHouseCard({ house }: { house: any }) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardContent>
+        <Typography gutterBottom variant='h5' component='div'>
+          {house.name}
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          Price: €{house.price.toLocaleString()}
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          Type: {house.type}
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          Area: {house.area}
+        </Typography>
+        <Button size='small'>View Details</Button>
+      </CardContent>
+    </Card>
+  );
+}
