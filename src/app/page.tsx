19cdf7b20db812dd791254180cd956090e8429a5
Merab Tskhvedadze <merabi.tskhvedadze@gmail.com>
2025-05-11 14:10:11 -0700

    add types for house data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,18 @@
 import Image from 'next/image';
-import FeaturedHouseCard from '@/components/featuredHouseCard';
+import FeaturedHouseCard from '@/components/FeaturedHouseCard';
 
 export default function Home() {
   return (
     <div>
       <FeaturedHouseCard
         house={{
+          id: 1,
           name: 'Beautiful House',
           price: 500000,
-          type: 'Villa',
+          type: 'villa',
           area: '200 m²',
+          description:
+            'A luxurious villa with modern amenities and a spacious garden.',
         }}
       />
     </div>
